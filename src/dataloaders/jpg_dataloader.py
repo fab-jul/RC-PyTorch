b48d546a06192ea5cf6e5b48a5185fbc40c9a901
Fabian <fmentzer@me.com>
2021-01-01 20:00:28 +0100

    Fix license

--- src/dataloaders/jpg_dataloader.py
+++ src/dataloaders/jpg_dataloader.py
@@ -1,20 +1,20 @@
 """
 Copyright 2020, ETH Zurich
 
-This file is part of L3C-PyTorch.
+This file is part of RC-PyTorch.
 
-L3C-PyTorch is free software: you can redistribute it and/or modify
+RC-PyTorch is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 any later version.
 
-L3C-PyTorch is distributed in the hope that it will be useful,
+RC-PyTorch is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with L3C-PyTorch.  If not, see <https://www.gnu.org/licenses/>.
+along with RC-PyTorch.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import numpy as np
